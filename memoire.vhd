d41e4f7410fb05f07e1a1481b29e59b7c95a2d40
Paul <paul.chauviere@gmail.com>
2021-12-05 17:14:16 +0100

    memoire terminé

--- memoire.vhd
+++ memoire.vhd
@@ -77,7 +77,22 @@ entity ecriture is
 end entity;
 
 architecture arch_ecriture of ecriture is
+    signal demi0, demi1, octet0, octet1, octet2, octet3 : std_logic_vector(31 downto 0);
 begin
+    demi0 <= current_mem(31 downto 16) & data_in(15 downto 0);
+    demi1 <= current_mem(31 downto 16) & data_in(31 downto 16);
+    octet0 <= current_mem(31 downto 8) & data_in(7 downto 0);
+    octet1 <= current_mem(31 downto 8) & data_in(15 downto 8);
+    octet2 <= current_mem(31 downto 8) & data_in(23 downto 16);
+    octet3 <= current_mem(31 downto 8) & data_in(31 downto 24);
+
+    data_out <= data_in when WriteMem_W = '1' else
+                demi0 when WriteMem_H = '1' and address(0) = '0' else 
+                demi1 when WriteMem_H = '1' and address(0) = '1' else 
+                octet0 when WriteMem_B = '1' and address(1 downto 0) = "00" else
+                octet1 when WriteMem_B = '1' and address(1 downto 0) = "01" else
+                octet2 when WriteMem_B = '1' and address(1 downto 0) = "10" else
+                octet3 when WriteMem_B = '1' and address(1 downto 0) = "11";
 
 end arch_ecriture;
 
@@ -98,7 +113,7 @@ USE IEEE.NUMERIC_STD.ALL;
 
 entity memoire is
     port(
-    address, data_in, current_mem : in std_logic_vector(31 downto 0);
+    address, data_in: in std_logic_vector(31 downto 0);
     data_out : out std_logic_vector(31 downto 0);
     ReadMem_W, ReadMem_SH, ReadMem_UH, ReadMem_SB, ReadMem_UB, WriteMem_W, WriteMem_H, WriteMem_B, WE, OE, clk : in std_logic
     );
@@ -106,6 +121,29 @@ entity memoire is
 end entity;
 
 architecture arch_memoire of memoire is
+    type tab2048x32 is array(2047 downto 0) of std_logic_vector(31 downto 0);
+    signal tab : tab2048x32;
+    signal mot,data_write, data_read: std_logic_vector(31 downto 0);
 begin
+    mot <= tab(to_integer(unsigned(address)));
+    read : Entity work.lecture port map (address, mot, data_read, ReadMem_W, ReadMem_SH, ReadMem_UH, ReadMem_SB, ReadMem_UB);
+    write : Entity work.ecriture port map (address, data_in, mot, data_write, WriteMem_W, WriteMem_H, WriteMem_B);
+
+P_ALU : process(CLK)
+
+    begin
+    if rising_edge(CLK) then
+        if (WE = '1') then
+            data_out <= data_read;
+        else 
+            tab(to_integer(unsigned(address))) <= data_write;
+            data_out <= data_write;
+        end if; 
+
+        if (OE = '1') then
+            data_out <= (others => 'Z');
+        end if;
+    end if;
+end process P_ALU;   
 
 end arch_memoire;
